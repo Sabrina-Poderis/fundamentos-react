39b5cd6ec27088dfa9f03c53154c4f12853ec9b2
Sabrina Poderis <sabrina.poderis@gmail.com>
2022-04-13 18:36:49 -0300

    Componente com Propriedade

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+// Componentes
 import Primeiro from './components/basics/Primeiro'
+import ComParametro from './components/basics/ComParametro'
 const root = document.getElementById('root')
 
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro 
+            titulo="Situação do aluno"
+            aluno="Sabrina"
+            nota={6.0}>
+        </ComParametro>
+        <ComParametro 
+            titulo="Situação do aluno"
+            aluno="Maria"
+            nota={7.0}>
+        </ComParametro>
     </div>, 
     root
 )
