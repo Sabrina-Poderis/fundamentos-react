de4ac99205575d515416a1d19e1f93228157b6aa
Sabrina Poderis <sabrina.poderis@gmail.com>
2022-04-13 20:26:21 -0300

    Componente Card #02

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css"
 import React from "react"
 
 // Componentes
@@ -9,24 +10,26 @@ import Card from './components/layouts/Card'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (props) => 
-    <div id="app">
-        <Card titulo="Desafio aleatório">
-            <Aleatorio min={1} max={100}></Aleatorio>
-        </Card>
-        <Card titulo="Fragmento">
-            <Fragmento></Fragmento>
-        </Card>
-        <Card titulo="Fragmento">
-            <Fragmento></Fragmento>
-        </Card>
-        <Card titulo="Com parâmetro">
-            <ComParametro 
-                titulo="Situação do aluno"
-                aluno="Sabrina"
-                nota={6.0}>
-            </ComParametro>
-        </Card>
-        <Card titulo="Primeiro componente">
-            <Primeiro></Primeiro>
-        </Card>
+    <div className="App">
+        <div className="Cards">
+            <Card titulo="Desafio aleatório">
+                <Aleatorio min={1} max={100}></Aleatorio>
+            </Card>
+            <Card titulo="Fragmento">
+                <Fragmento></Fragmento>
+            </Card>
+            <Card titulo="Fragmento">
+                <Fragmento></Fragmento>
+            </Card>
+            <Card titulo="Com parâmetro">
+                <ComParametro 
+                    titulo="Situação do aluno"
+                    aluno="Sabrina"
+                    nota={6.0}>
+                </ComParametro>
+            </Card>
+            <Card titulo="Primeiro componente">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
