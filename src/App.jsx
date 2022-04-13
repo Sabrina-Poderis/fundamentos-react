4e2d500c1f6fd9d5f2ee74ebcd6ddc3517897149
Sabrina Poderis <sabrina.poderis@gmail.com>
2022-04-13 20:35:26 -0300

    Componente Card #03

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,20 @@ import Card from './components/layouts/Card'
 export default (props) => 
     <div className="App">
         <div className="Cards">
-            <Card titulo="Desafio aleatório">
+            <Card titulo="Desafio aleatório" color="#a9e5bb">
                 <Aleatorio min={1} max={100}></Aleatorio>
             </Card>
-            <Card titulo="Fragmento">
+            <Card titulo="Fragmento" color="#D33E43">
                 <Fragmento></Fragmento>
             </Card>
-            <Card titulo="Fragmento">
-                <Fragmento></Fragmento>
-            </Card>
-            <Card titulo="Com parâmetro">
+            <Card titulo="Com parâmetro" color="#85FF9E">
                 <ComParametro 
                     titulo="Situação do aluno"
                     aluno="Sabrina"
                     nota={6.0}>
                 </ComParametro>
             </Card>
-            <Card titulo="Primeiro componente">
+            <Card titulo="Primeiro componente" color="#F4978E">
                 <Primeiro></Primeiro>
             </Card>
         </div>
--- src/components/layouts/Card.jsx
+++ src/components/layouts/Card.jsx
@@ -2,10 +2,18 @@ import "./Card.css"
 import React from "react"
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (props) => 
-    <div className="Card">
+export default (props) => {
+    const cardStyle = {
+        backgroundColor: props.color,
+        borderColor: props.color
+    }
+
+    return (
+        <div className="Card" style={cardStyle}>
         <h1 className="Title">{props.titulo}</h1>
         <div className="Content">
             {props.children}
         </div>
     </div>
+    )
+}
